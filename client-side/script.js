a7904593161a7a4fea33263f1b6a5ed73c22640e
adzanidham <adzani234@gmail.com>
2020-11-16 16:04:19 +0700

    toogle grid or single view

--- client-side/script.js
+++ client-side/script.js
@@ -1,57 +1,13 @@
 function _(el) {
-  return document.getElementById(el)
+  return document.querySelector(el)
 }
 
-_('upload_btn').addEventListener('click', function (e) {
-  e.preventDefault()
-  const form_data = new FormData(_('upload_form'))
-  // form_data.forEach((val, key) => console.log(key, val))
-
-  const xhr = new XMLHttpRequest()
-  xhr.upload.addEventListener('progress', progressHandler, false)
-  xhr.addEventListener('load', completeHandler, false)
-  xhr.addEventListener('error', errorHandler, false)
-  xhr.addEventListener('abort', abortHandler, false)
-  xhr.open('POST', 'http://localhost:3000/api/image')
-  xhr.send(form_data)
+// set single gallery
+_('.footer__menu--single').addEventListener('click', () => {
+  _('#imgGallery').classList.replace(_('#imgGallery').className, 'image__gallery--single')
 })
 
-function progressHandler(e) {
-  _('loaded_n_total').innerHTML = `Uploaded ${e.loaded} bytes of ${e.total}`
-
-  let percent = (e.loaded / e.total) * 100
-  let progress = Math.round(percent)
-  _('progres_bar').value = progress
-  _('upload_status').innerHTML = `${progress}% uploaded...`
-}
-
-function completeHandler(e) {
-  _('upload_status').innerHTML = `File uploaded successfully`
-}
-
-function errorHandler(e) {
-  _('upload_status').innerHTML = `File uploaded failed`
-}
-
-function abortHandler(e) {
-  _('upload_status').innerHTML = `File uploaded aborted`
-}
-
-function getImageGallery() {
-  fetch('http://localhost:3000/api/image')
-    .then(res => res.json())
-    .then(res => updateUI(res.values))
-  // .then(res => console.log(res.values))
-}
-
-function updateUI(images) {
-  let cards = ''
-  images.forEach(image => cards += tampilCards(image))
-  _('gallery').innerHTML = cards
-}
-
-function tampilCards(image) {
-  return `<img src="http://localhost:3000/uploaded-img/${image.img_name}" alt="${image.description}" width="100">`
-}
-
-getImageGallery()
+// set grid gallery
+_('.footer__menu--grid').addEventListener('click', () => {
+  _('#imgGallery').classList.replace(_('#imgGallery').className, 'image__gallery--grid')
+})
